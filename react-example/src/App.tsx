ade1b05b5500f2c9c3a0dd70da8ea84d115244a0
simplysylvia <sylvia.the.dev@gmail.com>
2024-04-23 07:22:37 -0700

    💥 removes code for follow along

--- react-example/src/App.tsx
+++ react-example/src/App.tsx
@@ -5,18 +5,11 @@ import HorizontalCardGallery from "./Components/UI/Cards/HorizontalCardGallery";
 import products_upcoming from "./db/mocks/products_upcoming.json";
 import products_sale from "./db/mocks/products_sale.json";
 
-// Bring in the useFlags hook from the LaunchDarkly SDK, so we can track our feature flags
-import { useFlags } from "launchdarkly-react-client-sdk";
-
 function App() {
-  // Use the useFlags hook to get the value of the feature flag
-  const flags = useFlags();
-
   return (
     <div className="App">
       <HorizontalCardGallery title={"🕐 Upcoming Releases"} products={products_upcoming} />
-      {/* Conditional for validating the boolean value of the flag */}
-      {flags.salesHighlight && <HorizontalCardGallery title={"Save some money with this 🔥 sale!"} products={products_sale}/>}
+      <HorizontalCardGallery title={"Save some money with this 🔥 sale!"} products={products_sale}/>
     </div>
   );
 }
--- react-example/src/index.tsx
+++ react-example/src/index.tsx
@@ -4,41 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Importing the Launch Darkly provider to wrap our app for access to feature flags
-import { asyncWithLDProvider, LDContext } from "launchdarkly-react-client-sdk";
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 
-// We are wrapping in an async iffy to use async/await
-(async ()=> {
-  // create the content for the Launch Darkly provider
-  const context : { clientSideID: string, context: LDContext } = {
-    clientSideID: "YourClientSideID",
-    context: {
-      // REQUIRED: what type of context are we setting
-      kind: "user",
-      // REQUIRED: add a unique key for tracking that context (every unique key will be tracked)
-      key: "user-access-ExampleUser",
-      // OPTIONAL: define custom attributes for the user context to track in LD
-      username: "ExampleUser",
-      region: "US",
-      enrolledInBeta: true,
-    }
-  };
+root.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
+);
 
-  // pass the context into the Provider
-  const  LDProvider =  await asyncWithLDProvider(context);
-
-  const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-  );
-
-  root.render(
-    <React.StrictMode>
-      {/* Wrap application with provider so feature flags are available */}
-      <LDProvider>
-        <App />
-      </LDProvider>
-    </React.StrictMode>
-  );
-
-  reportWebVitals();
-})();
+reportWebVitals();
